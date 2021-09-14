8c320e8c2b3172e5143ee9c53e4982daaeea6470
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-09-14 20:30:02 +0300

    Delete 2b_A. Количество равных максимальному.py

--- 2b_A. Количество равных максимальному.py
+++
@@ -1,9 +0,0 @@
-my_dict = dict()
-a = int(input())
-while a != 0:
-    if a not in my_dict.keys():
-        my_dict[a] = 1
-    else:
-        my_dict[a] += 1
-    a = int(input())
-print(my_dict[max(my_dict.keys())])
